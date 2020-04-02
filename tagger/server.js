38aa6dbb52272e5e04aad1f813b35e4f11575759
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2020-04-02 20:36:31 +0200

    Template engine (EJS) for tagger tool pages

--- tagger/server.js
+++ tagger/server.js
@@ -6,37 +6,27 @@ const data_dir = '../data/jmb22/real';
 
 const app = express();
 
-const makeAsync = fn => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
+app.set('views', '.');
+app.set('view engine', 'ejs');
 
-app.get('/', makeAsync(async (req, res) => {
-    const ids = await fs.readdir(data_dir)
-        .then(files => files.filter(file => file.endsWith('.png')))
-        .then(files => files.map(file => file.slice(0, file.length-4)));
-    const trans = await Promise.all(ids.map(async id => {
+async function list_trans (dir) {
+    const files = await fs.readdir(dir);
+    const ids = files
+        .filter(file => file.endsWith('.png'))
+        .map(file => file.slice(0, file.length-4));
+    return await Promise.all(ids.map(async id => {
         const data = await fs.readJson(`${data_dir}/${id}.json`);
-        return { id, meaning: data.meanings[0] };
+        return { id, meanings: data.meanings };
     }));
-    res.send(`<!doctype html>
-        <html>
-        <head>
-            <style>img { width: 200px; }</style>
-        </head>
-        <body>
-            <ul>`+
-        trans.map(({ id, meaning }) =>
-            `<li>${id}: ${meaning}<img src="img/${id}.png" /></li>`)
-            .join('')
-        +`  </ul>
-        </body>
-        </html>
-    `);
-}));
+}
 
-app.get('/img/:img', (req, res) => {
-    res.sendFile(req.params.img, { root: data_dir });
+app.get('/', (req, res, next) => {
+    list_trans(data_dir)
+        .then(trans => res.render('lista', { trans }))
+        .catch(next);
 });
+
+app.get('/img/:img', (req, res) => res.sendFile(req.params.img, { root: data_dir }));
 
 app.listen(3000, () => {
     console.log(`App started at http://localhost:3000`);
