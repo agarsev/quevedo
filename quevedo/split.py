2de8f6a850a7079e0364762007ea5dddd3dbee85
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2021-09-16 20:37:28 +0200

    feat: Specify a different range for folds in split command

--- quevedo/split.py
+++ quevedo/split.py
@@ -11,13 +11,24 @@ from quevedo.annotation import Target
 @click.command('split')
 @click.option('--grapheme-set', '-g', multiple=True, help="Grapheme set(s) to split.")
 @click.option('--logogram-set', '-l', multiple=True, help="Logogram set(s) to split.")
+@click.option('--start-fold', '-s', type=click.INT, default=0,
+              help='Minimum number to use for the folds.')
+@click.option('--end-fold', '-e', type=click.INT,
+              help='Maximum number to use for the folds.')
 @click.option('--seed', type=click.INT, help='A seed for the random split algorithm.')
 @click.pass_obj
-def split(obj, grapheme_set, logogram_set, seed):
+def split(obj, grapheme_set, logogram_set, start_fold, end_fold, seed):
     '''Assign annotations randomly to different folds.
 
-    Configure the number of folds in the dataset configuration file, as well as
-    which folds are to be used for training and which for testing.
+    By default, the annotations will be split into a number of folds configured
+    in the dataset configuration file, starting from 0. To override this, use
+    the `-s` and `-e` option to change the range of values for the folds.  This
+    can be used to ensure some particular subset of annotations is always
+    assigned to some fold, for example one that will always be used for train or
+    test.
+
+    Which folds are to be used for training and which for testing can be
+    configured in the dataset configuration file.
 
     If neither `-g` nor `-l` are used, all annotations in the dataset will be
     split, and if the special value "_ALL_" for either grapheme or logogram sets
@@ -28,7 +39,10 @@ def split(obj, grapheme_set, logogram_set, seed):
     them.'''
 
     dataset = obj['dataset']
-    number = dataset.config['folds']
+    if end_fold is None:
+        end_fold = dataset.config['folds']
+    else:
+        end_fold = end_fold + 1  # Exclusive ranges
 
     random.seed(seed)
 
@@ -47,6 +61,7 @@ def split(obj, grapheme_set, logogram_set, seed):
 
     an = list(an)
     random.shuffle(an)
+    number = end_fold - start_fold
     amount = (len(an) // number)+((len(an) % number) > 0)
 
     start = 0
@@ -55,7 +70,7 @@ def split(obj, grapheme_set, logogram_set, seed):
         if end > len(an):
             end = len(an)
         for t in an[start:end]:
-            t.fold = n
+            t.fold = start_fold + n
             t.save()
         remainder = end-start
         start += amount
